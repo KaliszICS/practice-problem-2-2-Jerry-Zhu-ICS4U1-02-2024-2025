a97f9d31b88e2de5f77459c25f5e4933dc5c2fd9
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-19 21:48:49 +0000

    Wip

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,36 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 		
 	}
-	
+	public static int[] compareSearch(int[] arr, int num){
+		int linearCounter = 0;
+for (int i = 0; i < arr.length; i++){
+	linearCounter = i + 1;
+	if(arr[i] == num){
+		i = arr.length;
+	}
+}
+
+		int binaryCounter = 0;
+		int high = arr.length-1;
+		int low = 0;
+		int mid;
+		while(low <= high){
+			mid = (low + high)/2;
+			binaryCounter++;
+			if(arr[mid] == num){
+				low = high + 1;
+			}
+			else if(arr[mid] < num){
+				high = mid - 1;
+			}
+			else{
+				low = mid + 1;
+			}
+		}
+		int[] arr2 = new int[] {linearCounter, binaryCounter};
+		return arr2;
+	}
+public static int[] compareSearchString(String[] arr, String phrase){
+
+}
 }
