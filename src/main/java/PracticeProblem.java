6e0dd8dd923ac06647e88410a5d2f596a4fe945f
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-19 22:02:48 +0000

    Finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -18,21 +18,47 @@ for (int i = 0; i < arr.length; i++){
 		int mid;
 		while(low <= high){
 			mid = (low + high)/2;
-			binaryCounter++;
 			if(arr[mid] == num){
 				low = high + 1;
 			}
 			else if(arr[mid] < num){
-				high = mid - 1;
-			}
-			else{
 				low = mid + 1;
 			}
+			else{
+				high = mid - 1;
+			}
+			binaryCounter++;
 		}
 		int[] arr2 = new int[] {linearCounter, binaryCounter};
 		return arr2;
 	}
-public static int[] compareSearchString(String[] arr, String phrase){
-
+public static int[] compareStringSearch(String[] arr, String phrase){
+	int linearCounter = 0;
+	for (int i = 0; i < arr.length; i++){
+		linearCounter = i + 1;
+		if(arr[i].equals(phrase)){
+			i = arr.length;
+		}
+	}
+	
+			int binaryCounter = 0;
+			int high = arr.length-1;
+			int low = 0;
+			int mid;
+			while(low <= high){
+				mid = (low + high)/2;
+				if(arr[mid].equals(phrase)){
+					low = high + 1;
+				}
+				else if(arr[mid].compareTo(phrase) < 0){
+					low = mid + 1;
+				}
+				else{
+					high = mid - 1;
+				}
+				binaryCounter++;
+			}
+			int[] arr2 = new int[] {linearCounter, binaryCounter};
+			return arr2;
 }
 }
